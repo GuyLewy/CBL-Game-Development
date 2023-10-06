a782db8f531dbdd8652c8c735a6fba5af890e97c
Guy Lewy <98020734+GuyLewy@users.noreply.github.com>
2023-10-06 14:33:13 +0200

    Player and frame rendering complete, player movement added TODO: fix player collision with bottom of screen

---
+++ DisplayGraphics.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * .
+ */
+public class DisplayGraphics extends Canvas {
+
+    public static int windowHeight = 0;
+
+    public static void main(String[] args) {
+        Player player = new Player();
+        JFrame mainWindow = new JFrame();
+        mainWindow.add(player);
+        mainWindow.setSize(400, 400);
+        mainWindow.setVisible(true);
+        windowHeight = mainWindow.getBounds().height;
+        System.out.println(windowHeight);
+    }
+}
---
+++ Player.java
@@ -0,0 +1,79 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.*;
+
+/**
+ * .
+ */
+public class Player extends JPanel implements KeyListener, ActionListener {
+    public static final int MOVEMENT_SPEED = 1;
+
+    int playerY = 100;
+    boolean upPressed = false;
+    boolean downPressed = false;
+
+    Timer t = new Timer(5, this);
+
+    /**
+     * .
+     */
+    public Player() {
+        t.start();
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+    }
+
+    /**
+     * .
+     */
+    public void actionPerformed(ActionEvent e) {
+        repaint();
+        if (upPressed && playerY > 0) {
+            playerY -= MOVEMENT_SPEED;
+        }
+        if (downPressed && playerY + 100 < DisplayGraphics.windowHeight) {
+            playerY += MOVEMENT_SPEED;
+        }
+    }
+
+    /**
+     * .
+     */
+    public void keyPressed(KeyEvent e) {
+        int code = e.getKeyCode();
+        if (code == KeyEvent.VK_UP) {
+            upPressed = true;
+        } else if (code == KeyEvent.VK_DOWN) {
+            downPressed = true;
+        }
+    }
+
+    /**
+     * .
+     */
+    public void keyReleased(KeyEvent e) {
+        int code = e.getKeyCode();
+
+        if (code == KeyEvent.VK_UP) {
+            upPressed = false;
+        } else if (code == KeyEvent.VK_DOWN) {
+            downPressed = false;
+        }
+    }
+
+    /**
+     * .
+     */
+
+    public void paint(Graphics g) {
+        setForeground(Color.black);
+        g.fillRect(100, playerY, 100, 100);
+    }
+
+    public void keyTyped(KeyEvent e) {
+    }
+}
